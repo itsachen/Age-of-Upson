eb954f318739c966f3088095539c3ca5d3bf2bbe
Anthony Chen <pandaman0529@gmail.com>
2012-04-22 20:52:27 -0400

    Changing state's purpose

--- game/state.ml
+++ game/state.ml
@@ -1,6 +1,31 @@
 open Definitions
 open Constants
 open Util
-open Game
 
-let getBuildings (g: game)
+type team = {color: color; score:score; units:unit_data list ref; 
+   buildings:building_data list ref; age:age; food:food_count; wood:wood_count; 
+   upgrades:upgrades}
+
+type state= {team_red: team; team_blue:team; 
+   resources: resource_data list; timer: int ref}
+
+let getBuildings (g: game) (c: color) : building_data list=
+   let result= g.team_red.buildings in
+   Mutex.lock g.m;
+   if g.team_red.color = c then (Mutex.unlock g.m; !result)
+   else (Mutex.unlock g.m; !(g.team_blue.buildings))
+
+let getTeamStatus (s: state) (c: color): team_data=
+   failwith "Not implemented yet!"
+
+let getUnitStatus (s: state) (i: unit_id): unit_data=
+   failwith "Not implemented yet!"
+
+let getBuildingStatus (s: state) (i: building_id): building_data=
+   failwith "Not implemented yet!"
+
+let getGameStatus (s: state) : game_data=
+   failwith "Not implemented yet!"
+
+let getResourceStatus (s: state) : game_data=
+   failwith "Not implemented yet!"
--- game/state.mli
+++ game/state.mli
@@ -1,10 +1,23 @@
+open Definitions
+open Constants
+open Util
 
-val getBuildings: game -> color -> building_data
+val getBuildings: game -> color -> building_data list
 
-val getUnits: game -> color -> unit_data
+val getUnits: game -> color -> unit_data list
 
 val getResources: game -> resource_data list
 
 val getScore: game -> int
 
 val getTime: game -> float
+
+val getTeamStatus: state -> color -> team_data
+
+val getUnitStatus: state -> unit_id -> unit_data
+
+val getBuildingStatus: state -> building_id -> building_data
+
+val getGameStatus: state -> game_data
+
+val getResourceStatus: state -> game_data
