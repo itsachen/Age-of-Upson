ac7d77a06889a98a27d1e05a175b5b9162a82f69
Anthony Chen <pandaman0529@gmail.com>
2012-04-30 16:18:58 -0400

    Started queuefunctions

--- game/hashqueue.ml
+++ game/hashqueue.ml
@@ -1,6 +1,7 @@
 open Definitions
 open Constants
 open Util
+open Unit
 
 type id= 
    UnitId of unit_id |
@@ -19,3 +20,55 @@ let createHashq (n: int) : hashqueue =
 	Hashtbl.create n
 
 (* Getter and setter methods *)
+
+(* q is a ref *)
+let queueCollect uid q c s: result=
+   (* Check if uid is of same color*)
+   let col= getTeam uid s in
+   match col with
+   | Some(cresult) -> 
+      (if c = cresult then 
+         (let ty= getType uid s in
+          match ty with
+          | Some(tyresult) -> 
+             if ty = Villager then (
+                let oldq= Hashtbl.find q uid in
+                Queue.add uid oldq in
+                Hashtbl.replace q uid oldq in
+                Success)
+             else Success
+          | None -> Failed (* uid does not exist *) )
+       else Failed (* unit belongs to other team *))
+   | None -> Failed (* uid does not exist *)
+
+let queueMove movtup q c s: result=
+   (* Check if uid is of same color *)
+   let col= getTeam uid s in
+   match col with
+   | Some(cresult) ->
+      (if c = cresult then
+         (let oldq= Hashtbl.find q uid in
+          Queue.add movtup oldq in
+          Hashtbl.replace q uid oldq in
+          Success)
+       else Failed (* unit belongs to other team *))
+   | None -> Failed (* uid does not exist *)
+
+let queueAttack atttup q c s: result=
+   (* Check if uid is of same color*)
+   let col= getTeam uid s in
+   match col with
+   | Some(cresult) -> 
+      (if c = cresult then 
+         (let ty= getType uid s in
+          match ty with
+          | Some(tyresult) -> 
+             if ty = Villager then (
+                let oldq= Hashtbl.find q uid in
+                Queue.add uid oldq in
+                Hashtbl.replace q uid oldq in
+                Success)
+             else Success
+          | None -> Failed (* uid does not exist *) )
+       else Failed (* unit belongs to other team *))
+   | None -> Failed (* uid does not exist *)
