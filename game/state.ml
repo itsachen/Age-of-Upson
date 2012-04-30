98ab9d4907570152069ac4bdd03a8ff3bbd95688
Anthony Chen <pandaman0529@gmail.com>
2012-04-30 16:55:17 -0400

    Moved unit methods to state

--- game/state.ml
+++ game/state.ml
@@ -148,6 +148,50 @@ let setBuildq (s:state) (q: hashqueue):unit =
 	
 let setSpawnq (s:state) (q: hashqueue):unit =
 	s.spawnq := q; ()
+
+(* CHANGE BOTH TO LIST.FIND_ALL *)
+(* Returns the team that the UID is. *)
+let getTeam uid s: color option=
+   let foo= List.fold_left
+      (fun a c ->
+         match c with
+         | (id,ty,h,p) -> if id = uid then c else a) 
+      (-1,Villager,-1,(-1.0,-1.0)) !(s.team_red.units) in
+   if foo = (-1,Villager,-1,(-1.0,-1.0)) then
+      (let foo2= List.fold_left 
+         (fun a c ->
+            match c with
+            | (id,ty,h,p) -> if id = uid then c else a) 
+      (-1,Villager,-1,(-1.0,-1.0)) !(s.team_blue.units) in
+      if foo2= (-1,Villager,-1,(-1.0,-1.0)) then None
+      else Some(Blue))
+   else Some(Red)
+
+let getType uid s: unit_type option=
+   let foo= List.fold_left
+      (fun a c ->
+         match c with
+         | (id,ty,h,p) -> if id = uid then c else a) 
+      (-1,Villager,-1,(-1.0,-1.0)) !(s.team_red.units) in
+   if foo = (-1,Villager,-1,(-1.0,-1.0)) then
+      (let foo2= List.fold_left 
+         (fun a c ->
+            match c with
+            | (id,ty,h,p) -> if id = uid then c else a) 
+      (-1,Villager,-1,(-1.0,-1.0)) !(s.team_blue.units) in
+      if foo2= (-1,Villager,-1,(-1.0,-1.0)) then None
+      else let (_,ty,_,_)= foo2 in Some(ty))
+   else let (_,ty,_,_)= foo in Some(ty)
+
+let getIsBuilding uid s: bool=
+   let foo= List.filter (fun (id,ty,h,til) -> uid = id) s.team_red.buildings in
+   match foo with
+   | [] -> 
+      (let foo2= List.filter
+      (fun (id,ty,h,til) -> uid = id) s.team_blue.buildings in
+      if foo2 = [] then false else true)
+   | _ -> true
+   
 	
 	
 			
