d2aa040cc3f3d7d748efa9d39f725dccf0059257
Anthony Chen <pandaman0529@gmail.com>
2012-04-30 17:14:42 -0400

    Finished getters/setters for queue

--- game/hashqueue.ml
+++ game/hashqueue.ml
@@ -45,9 +45,9 @@ let queueMove movtup q c copt s: result=
    match copt with
    | Some(cresult) ->
       (if c = cresult then
-         (let oldq= Hashtbl.find q uid in
+         (let oldq= Hashtbl.find q (fst movtup) in
           Queue.add movtup oldq;
-          Hashtbl.replace q uid oldq;
+          Hashtbl.replace q (fst movtup) oldq;
           Success)
        else Failed (* unit belongs to other team *))
    | None -> Failed (* uid does not exist *)
@@ -86,19 +86,43 @@ let queueBuild buildtup q c copt tyopt s: result=
        else Failed (* unit belongs to other team *))
    | None -> Failed (* uid does not exist *)
 
-let queueSpawn spawntup q c copt tyopt s: result=
+let queueSpawn spawntup q c copt isbuild s: result=
+   (* Check if uid is of same color*)
+   match copt with
+   | Some(cresult) -> 
+      (if c = cresult then 
+         (if isbuild then (
+             let oldq= Hashtbl.find q (fst spawntup) in
+             Queue.add spawntup oldq;
+             Hashtbl.replace q (fst spawntup) oldq;
+             Success)
+          else Success (* Not a building, fool *))
+       else Failed (* unit belongs to other team *))
+   | None -> Failed (* uid does not exist *)
+
+let clearAttack uid q c copt tyopt s: result=
    (* Check if uid is of same color*)
    match copt with
    | Some(cresult) -> 
       (if c = cresult then 
          (match tyopt with
           | Some(tyresult) -> 
-             if tyresult = Villager then (
-                let oldq= Hashtbl.find q (fst buildtup) in
-                Queue.add buildtup oldq;
-                Hashtbl.replace q (fst buildtup) oldq;
+             if tyresult <> Villager then (
+                let emptqueue= Queue.create () in
+                Hashtbl.replace q uid emptqueue;
                 Success)
              else Success
           | None -> Failed (* uid does not exist *) )
+       else Failed (* unit belongs to other team *))
+   | None -> Failed (* uid does not exist *)
+
+let clearMove uid q c copt s: result=
+   (* Check if uid is of same color *)
+   match copt with
+   | Some(cresult) ->
+      (if c = cresult then
+         (let emptqueue= Queue.create () in 
+          Hashtbl.replace q uid emptqueue;
+          Success)
        else Failed (* unit belongs to other team *))
    | None -> Failed (* uid does not exist *)
